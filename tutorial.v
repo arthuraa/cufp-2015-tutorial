63794fdb1060ec635b639a94e4ac199c7c85613b
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2015-08-25 14:34:42 -0400

    Revert "Test."
    This reverts commit 833a754c42d69ac8d0ca1b9f3b6545031d114b8a.

--- tutorial.v
+++ tutorial.v
@@ -46,7 +46,6 @@ Definition andb (b1 b2: bool) : bool :=
   | false => false
   end.
 
-
 (** Let's test our functions. The [Compute] command tells Coq to
     evaluate an expression and print the result on the screen.*)
 
